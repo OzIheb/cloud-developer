0a07a3517c00979a9047506923f791ec8f044e8f
Iheb Ouelhazi <ouelhazi3iheb@gmail.com>
2022-11-03 21:20:49 +0100

    Fix error with http request

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -27,7 +27,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get("/filteredimage?image_url={{URL}}", async (req, res) => {
+  app.get("/filteredimage", async (req, res) => {
     let image_url = req.query;
     if (!image_url) {
       res.status(400).send("invalide image url");
